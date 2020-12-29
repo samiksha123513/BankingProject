7e289b31109a87739e0facdeb7a744ea0e4d46f4
prajakta <prajakta@prajakta-PC>
2020-12-29 17:52:49 +0530

    this is commit

--- BankingProject/src/test/java/banking/project/Page/HomePage.java
+++ BankingProject/src/test/java/banking/project/Page/HomePage.java
@@ -9,17 +9,36 @@ public class HomePage
 {
 	WebDriver Driver=null;
 	
-	@FindBy(xpath="//a[text()='here']")
+	/*@FindBy(xpath="//a[text()='here']")
 	WebElement Visit_here;
 
+	@FindBy(name="emailid")
+	WebElement EmailID;
+	
+	@FindBy(name="btnLogin")
+	WebElement Submit;
+*/	
+	@FindBy(name="uid")
+	WebElement UserID;
+	
+	@FindBy(name="password")
+	WebElement Password;
 	
 	public HomePage(WebDriver driver)
 	{
 		this.Driver=driver;
 		PageFactory.initElements(driver, this);	
 	}
-	public void visit()
+	/*public void visit(String Email)
 	{
 		Visit_here.click();	
+		EmailID.sendKeys(Email);
+		Submit.click();s
+		
+	}*/
+	public void Login(String userid,String password)
+	{
+		UserID.sendKeys(userid);
+		Password.sendKeys(password);
 	}
 }
